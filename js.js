11e9ae113925f2a05964e23ebdb8ceaa8e07177b
CunningCJ <chrisjw1291@gmail.com>
2022-08-23 17:31:30 +0100

    got a random choice

---
+++ js.js
@@ -0,0 +1,11 @@
+let getComputerChoice = Math.random();
+if (getComputerChoice <= 0.33){
+    console.log("Rock") 
+} else if (getComputerChoice >= 0.66)
+    {
+    console.log("Paper")
+} else {
+    console.log("Scissor")
+}
+console.log(getComputerChoice);
+
