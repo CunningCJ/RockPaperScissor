57c86c4c2da25e3fb6cc58a43db4a66f0239a1fa
CunningCJ <chrisjw1291@gmail.com>
2022-10-20 15:30:32 +0100

    finished ui for rock paper scissor

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
 let moves = 0;
+let ties = 0;
 
 function getComputerChoice(){ Math.random();
 if (Math.random() <= 0.33){
@@ -12,15 +13,57 @@ if (Math.random() <= 0.33){
     computerSelection = "scissor";
 }}
 
-getComputerChoice();
+document.getElementById("rock").addEventListener("click", function(){ 
+    playerSelection = 'rock';
+    go();
+});
+document.getElementById("paper").addEventListener("click", function(){ 
+    playerSelection = 'paper';
+    go();
+});
+document.getElementById("scissor").addEventListener("click", function(){ 
+    playerSelection = 'scissor';
+    go();
+});
+ 
+function go(){
+    getComputerChoice();
+    console.log(computerSelection);
+    playRound(playerSelection, computerSelection); 
+    document.getElementById("pS").textContent = "Player - Wins: " + playerScore;
+    document.getElementById("gC").textContent = "Games - " + moves;
+    document.getElementById("pcS").textContent = "AI - Wins: " + computerScore;
+    document.getElementById("tC").textContent = "Tied games - " + ties;
+    document.getElementById("winner").textContent = "" + result;
+    if (playerScore === 5) {
+        document.getElementById("winner").textContent = "Game Over - You are victorious. User("+playerScore+") vs Computer("+computerScore+")"
+        // alert("Game Over - You are victorious. User("+playerScore+") vs Computer("+computerScore+")");
+        } else if (computerScore === 5){
+        document.getElementById("winner").textContent = "Game Over - The computer wins! User("+playerScore+") vs Computer("+computerScore+")";
+    }
 
-console.log(computerSelection);
+}
+document.getElementById("reset").addEventListener("click", function(){
+    reset();
+})
 
-let playerSelection = prompt("Rock, Paper or Scissor").toLowerCase();
+function reset(){
+    playerScore = 0;
+    computerScore = 0;
+    moves = 0;
+    ties = 0;
+    document.getElementById("pS").textContent = "Player - Wins: " + playerScore;
+    document.getElementById("gC").textContent = "Games - " + moves;
+    document.getElementById("pcS").textContent = "AI - Wins: " + computerScore;
+    document.getElementById("tC").textContent = "Tied games - " + ties;
+    document.getElementById("winner").textContent = "" ;
+
+}
 
 function playRound(playerSelection, computerSelection){
 if (playerSelection === computerSelection){
     result = "Tie";
+    ties++;
     moves++;
 return;
 }
@@ -61,51 +104,18 @@ else if (playerSelection === "scissor" && computerSelection === "paper"){
     return ;
 }} 
 
-playRound(playerSelection, computerSelection);
-console.log(result);
-console.log("your score = " + playerScore);
-console.log("Computer's score = " + computerScore);
-console.log(moves);
 function game(playerSelection, computerSelection){
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissor").toLowerCase();
         getComputerChoice();
-        console.log(computerSelection);
         playRound(playerSelection, computerSelection);
-        console.log(result);
-        console.log("your score = " + playerScore);
-        console.log("Computer's score = " + computerScore);
-        console.log(moves);
         if (playerScore === 2) {
              console.log("Game Over=> User("+playerScore+") vs Computer("+computerScore+")");
              alert("Game Over - You are victorious");}
             else if (computerScore === 2){
                 console.log("Game Over=> User("+playerScore+") vs Computer("+computerScore+")");
                 alert("Game Over - The computer wins!");}
-   
             }
     }
 
-game(playerSelection, computerSelection);
+// game(playerSelection, computerSelection);
 
-/*
-var i = 0;
-const play = () => {
-    let playerSelection = prompt("Rock, Paper or Scissor").toLowerCase();
-    getComputerChoice();
-    console.log(computerSelection);
-    playRound(playerSelection, computerSelection);
-    console.log(result);
-    console.log("your score = " + playerScore);
-    console.log("Computer's score = " + computerScore);
-    console.log(moves);
-    i++;
-    if (i !== 5) {
-        play();
-    } else {
-        alert("Game Over=> User("+playerScore+") vs Computer("+computerScore+")");
-    }
-}
-
-play();
-*/
